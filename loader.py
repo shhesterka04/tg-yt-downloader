ab23598cf7e5eaaf4a3c3d2612c2e68b3e599a96
Eroshkin Artem <artemeroshkin47@gmail.com>
2023-02-12 13:32:59 +0300

    start work on project

---
+++ loader.py
@@ -0,0 +1,9 @@
+from aiogram import Bot, Dispatcher, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+
+token = 'TOKEN'
+
+bot = Bot(token=token, parse_mode=types.ParseMode.HTML)
+storage = MemoryStorage()
+
+dp = Dispatcher(bot, storage=storage)
