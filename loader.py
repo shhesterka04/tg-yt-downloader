9c4e5d505655e5a877f12c07bd574c1afc36b6f6
shesterka <88100981+shhesterka04@users.noreply.github.com>
2023-03-31 18:18:21 +0300

    create a base structure of project

--- loader.py
+++ loader.py
@@ -1,9 +1,10 @@
 from aiogram import Bot, Dispatcher, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
-token = 'TOKEN'
+token = ''
 
 bot = Bot(token=token, parse_mode=types.ParseMode.HTML)
+
 storage = MemoryStorage()
 
 dp = Dispatcher(bot, storage=storage)
